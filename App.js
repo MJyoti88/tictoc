2870a2f7226bf55faf6d211a2f277bb010498ba2
MJyoti88 <jyotimandeval9@gmail.com>
2024-10-11 16:07:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TicTocToe from './components/TicTocToe/TicTocToe';
+
+function App() {
+  return (
+    <div className="App">
+      <TicTocToe/>
+    </div>
+  );
+}
+
+export default App;
